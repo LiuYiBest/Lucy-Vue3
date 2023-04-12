fb98d8dd8f78aeab5d6a9c6a9f13031cfe3faa98
liuyidiao <1070692044@qq.com>
2023-04-12 20:48:27 +0800

    实现effect.schduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,6 @@
 class ReactiveEffect{
     private _fn:any;
-    constructor(fn){
+    constructor(fn,public scheduler?){
         this._fn = fn;
     }
     run(){
@@ -31,14 +31,19 @@ export function trigger(target,key){
     let depsMap = targeMap.get(target);
     let dep = depsMap.get(key);
     for (const effect of dep) {
-        effect.run();
+        if(effect.scheduler){
+            effect.scheduler()
+        }else{
+            effect.run();
+        }
     }
 }
 
 let activeEffect;
-export function effect(fn){
+export function effect(fn,options:any={}){
     // fn
-    const _effect = new ReactiveEffect(fn);
+    const scheduler = options.scheduler;
+    const _effect = new ReactiveEffect(fn,scheduler);
     _effect.run()
 
     return _effect.run.bind(_effect)
