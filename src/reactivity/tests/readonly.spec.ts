ac45498383e5118494184c1103ddb82f2f90a026
liuyidiao <1070692044@qq.com>
2023-04-21 21:20:40 +0800

    readoly.spec

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,5 +1,8 @@
 describe("readonly", () => {
     it("happy path",()=>{
         const original = {foo:1,bar:{baz:2}};
+        const wrapped =readonly(original);
+        expect(wrapped).not.toBe(original);
+        expect(wrapped.foo).toBe(1);
     })
 })
